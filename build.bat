1c4d1c253f0dbce2c5891355a818a89a2e855fd6
Max Base <MaxBaseCode@Gmail.Com>
2019-05-16 07:07:36 +0430

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-@g++ main.cpp -o main.exe sfml-graphics-d-2.dll sfml-system-d-2.dll sfml-window-d-2.dll -ISFML/include
+@g++ main.cpp -o main.exe SFML/bin/sfml-graphics-d-2.dll SFML/bin/sfml-system-d-2.dll SFML/bin/sfml-window-d-2.dll -ISFML/include
